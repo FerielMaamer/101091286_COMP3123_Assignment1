71e7e364a2fe84b6fdbc4b35f0778dd29c2d5dcd
FerielMaamer <ferielmaamer@yahoo.fr>
2022-10-23 17:13:49 -0400

    password encryption

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,33 @@
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = new mongoose.Schema({
-    username : {type: String, maxLength: 100},
-    email : {type: String, maxLength: 50, unique: true},
+    username : {primaryKey:true, type: String, maxLength: 100},
+    email : {
+        type: String, 
+        maxLength: 50, 
+        unique: true,
+        validate: [isEmail, 'invalid email'],
+    },
     password: {type: String, maxLength: 50}
 })
 
+userSchema.pre('save', async function save(next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
 
+userSchema.methods.validatePassword = async function validatePassword(data) {
+  return bcrypt.compare(data, this.password);
+};
 
 module.exports = mongoose.model("user", userSchema);
